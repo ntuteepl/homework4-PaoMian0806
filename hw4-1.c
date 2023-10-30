c3c6a8dc542ebca71b6f407006eff2fe49d3cdc3
PaoMian <84494844+PaoMian0806@users.noreply.github.com>
2023-10-30 19:17:23 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,35 @@
+int main()
+{
+    int num[50] = {0};
+    int n;
+    scanf("%d",&n);
+    if (n >= 1 && n <= 50){
+        for (int i = 0; i < n; i++){
+            scanf("%d",&num[i]);
+        }
+        for (int i = 0; i < n; i++){
+            int check = 0;
+            if (num[i] == 1){
+                printf("NO ");
+            }
+            else if (num[i] == 2){
+                printf("YES ");
+            }
+            else {
+                for (int j = num[i]-1; j > 1; j--){
+                    if (num[i]%j == 0){
+                        check = 1;
+                        printf("NO ");
+                        break;
+                    }
+                }
+                if (check == 0){
+                    printf("YES ");
+                }
+            }
+        }
+    }
+
+    return 0;
+}
 
